be013f4d8abf843ac91e5ac965f7ba3827552e50
Ayman Ramadan <aymanramadan677@gmail.com>
2023-11-09 22:46:53 +0200

    return publisherId and publisherName and Photo in Get Sotry bu Id

--- Medium.BL/Features/Stories/Mapping/GetStoryByIdMapping.cs
+++ Medium.BL/Features/Stories/Mapping/GetStoryByIdMapping.cs
@@ -10,6 +10,9 @@ namespace Medium.BL.Features.Stories.Mapping
             CreateMap<Story, GetStoryByIdResponse>()
                       .ForMember(s => s.StoryPhotos, options => options.MapFrom(s => s.StoryPhotos.Select(s => s.Url)))
                       .ForMember(s => s.StoryVideos, options => options.MapFrom(s => s.StoryVideos.Select(s => s.Url)))
+                      .ForMember(s => s.PublisherId, options => options.MapFrom(s => s.Publisher.Id))
+                      .ForMember(s => s.PublisherName, options => options.MapFrom(s => s.Publisher.Name))
+                      .ForMember(s => s.PublisherPhoto, options => options.MapFrom(s => s.Publisher.PhotoUrl))
                        .ForMember(s => s.Topics, options => options.MapFrom(s => s.Topics.Select(t => t.Name)))
                        .ForMember(s => s.ReactsCount, options => options.MapFrom(s => s.Reacts.Count()));
         }
--- Medium.BL/Features/Stories/Responses/GetAllStoryResponse.cs
+++ Medium.BL/Features/Stories/Responses/GetAllStoryResponse.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public int PublisherId { get; set; }
         public DateTime CreationDate { get; set; }
         public List<string>? StoryPhotos { get; set; }
         public List<string>? StoryVideos { get; set; }
