cdc68bc66a3c275cdf84af4949e413c21831e277
Abdulfattah Hessein <abdulfattah.hessein@gmail.com>
2023-11-14 13:24:26 +0200

    cancel validation on bio

--- Medium.BL/Features/Publisher/Validators/UpdatePublisherRequestValidator.cs
+++ Medium.BL/Features/Publisher/Validators/UpdatePublisherRequestValidator.cs
@@ -12,14 +12,8 @@ namespace Medium.BL.Features.Publisher.Validators
         {
             this._unitOfWork = unitOfWork;
 
-            RuleFor(p => p.Name).NotNull().
-                WithMessage("{PropertyName} Must be not Null")
-                .NotEmpty().WithMessage("{PropertyName}Must be not empty");
-
-            RuleFor(p => p.Bio).NotNull().
-                WithMessage("{PropertyName} Must be not Null")
+            RuleFor(p => p.Name).NotNull()
                 .NotEmpty().WithMessage("{PropertyName} Must be not empty");
-
         }
 
     }
